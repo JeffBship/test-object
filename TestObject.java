c429c339cacc292486b7fa29601821b3c86e2b52
Jeff Blankenship <jeffrey.blankenship@ccsu.edu>
2021-09-29 12:57:46 -0400

    initial upload

---
+++ TestObject.java
@@ -0,0 +1,39 @@
+//Jeff Blankenship
+//CS-151
+//TestObject provides user created objects for
+//parameter passing demonstrations.
+
+public class TestObject{
+  
+  //instance variable
+  String data;
+  
+  //constructor
+  public TestObject(){
+    this.data = new String("SomeDataGoesHere");
+  }
+  
+  //provide access to outside classes
+  public String getData(){
+    return this.data;
+  }
+  
+  //allow outside classes to update data
+  public void setData(String newData){
+    //basic error handling...null values are skipped
+    if (newData == null){ 
+      //do nothing, skipping the null data
+    } else {
+      //update data
+      this.data = newData;
+    }
+  }
+  
+}
+
+
+
+
+
+
+
